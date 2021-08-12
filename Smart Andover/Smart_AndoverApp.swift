fd6756634116b46a36bd23f7b534f71a7e256516
Chaniel Abou-Ezzi <chanielezzi@gmail.com>
2021-08-12 07:32:58 -0400

    Initial Commit

---
+++ Smart Andover/Smart_AndoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  Smart_AndoverApp.swift
+//  Smart Andover
+//
+//  Created by Chaniel Ezzi on 8/12/21.
+//
+
+import SwiftUI
+
+@main
+struct Smart_AndoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
